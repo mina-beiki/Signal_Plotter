1d02aaf7de4c4cd2ae94a8b5c30d47a8a2838429
mina-beiki <mina_beiki@yahoo.com>
2022-03-03 20:16:38 +0330

    added signals 1 and 2

--- main.py
+++ main.py
@@ -3,26 +3,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import style
 
-start = -10
-end = 10
-step = 0.01
-x1 = np.linspace(start, end, int((end-start)/step))
-y1 = np.sin(x1)
-#x1= np.linspace(start, end, num=(end-start)/step)
-#y1= [3, 4, 5, 6, 7, 8]
-
-#y2= [1, 2, 3, 4, 5, 6]
-#x2= [3, 4, 5, 6, 7, 8]
-
-style.use('ggplot')
-plt.plot(x1, y1, color='red', label='Plot1')
-#plt.plot(x2, y2, color='blue', label='PLot1')
-plt.title('Plots')
-plt.xlabel('X-Axis')
-plt.ylabel('Y-Axis')
-plt.legend()
-plt.show()
-
-#f, plt_arr=plt.subplot(3, sharex='col') #3 ta nemodar ke mehvare x ro share mikonan, f marboot be 3 ta nemoodar hast
-#f.suptitle('Plots')
-
+# Signal 1:
+# style.use('ggplot')
+# start = -np.pi
+# end = np.pi
+# step = 0.01
+# x1 = np.linspace(start, end, int((end - start) / step))
+# y1 = np.sin(x1)
+# plt.plot(x1, y1)
+# plt.title('1')
+# plt.xlabel('X-Axis')
+# plt.ylabel('Y-Axis')
+# plt.legend()
+# plt.show()
+# ----------------
+# Signal 2:
+# start1 = -5
+# end1 = -1
+# start2 = 0
+# end2 = 5
+# x1 = np.linspace(start1, end1, end1-start1+1)
+# y1 = -x1-1
+# x2 = np.linspace(start2, end2, end2-start2+1)
+# y2 = np.power(x2, 2)
+# style.use('ggplot')
+# plt.stem(x1, y1)
+# plt.stem(x2, y2)
+# plt.title('2')
+# plt.xlabel('X-Axis')
+# plt.ylabel('Y-Axis')
+# plt.legend()
+# plt.show()
